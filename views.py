906d72c87ceac5bd4af804627f13843989f1cdcc
ioan beilic <ioan@ioans-MacBook-Pro.local>
2026-03-07 13:18:23 +0100

    feat: convert drawer panels to full-page forms

--- views.py
+++ views.py
@@ -3,6 +3,8 @@ Quality Control Module Views
 """
 from django.core.paginator import Paginator
 from django.db.models import Q, Count
+from django.http import HttpResponse
+from django.urls import reverse
 from django.shortcuts import get_object_or_404, render as django_render
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
@@ -113,6 +115,7 @@ def inspections_list(request):
     }
 
 @login_required
+@htmx_view('quality/pages/inspection_add.html', 'quality/partials/inspection_add_content.html')
 def inspection_add(request):
     hub_id = request.session.get('hub_id')
     if request.method == 'POST':
@@ -134,10 +137,13 @@ def inspection_add(request):
         obj.result = result
         obj.notes = notes
         obj.save()
-        return _render_inspections_list(request, hub_id)
-    return django_render(request, 'quality/partials/panel_inspection_add.html', {})
+        response = HttpResponse(status=204)
+        response['HX-Redirect'] = reverse('quality:inspections_list')
+        return response
+    return {}
 
 @login_required
+@htmx_view('quality/pages/inspection_edit.html', 'quality/partials/inspection_edit_content.html')
 def inspection_edit(request, pk):
     hub_id = request.session.get('hub_id')
     obj = get_object_or_404(Inspection, pk=pk, hub_id=hub_id, is_deleted=False)
@@ -152,7 +158,7 @@ def inspection_edit(request, pk):
         obj.notes = request.POST.get('notes', '').strip()
         obj.save()
         return _render_inspections_list(request, hub_id)
-    return django_render(request, 'quality/partials/panel_inspection_edit.html', {'obj': obj})
+    return {'obj': obj}
 
 @login_required
 @require_POST
